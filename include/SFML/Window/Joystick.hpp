9376c8b93d579a53a45437b4c372d4597723bf86
Jason Brillante <jbrillante@anevia.com>
2019-05-14 16:24:23 +0200

    Increase the maximum amount of joysticks that can be handled.

--- include/SFML/Window/Joystick.hpp
+++ include/SFML/Window/Joystick.hpp
@@ -48,7 +48,7 @@ public:
     ////////////////////////////////////////////////////////////
     enum
     {
-        Count       = 8,  ///< Maximum number of supported joysticks
+        Count       = 12,  ///< Maximum number of supported joysticks
         ButtonCount = 32, ///< Maximum number of supported buttons
         AxisCount   = 8   ///< Maximum number of supported axes
     };
